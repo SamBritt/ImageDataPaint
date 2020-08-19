47c8373f26ec87d88093ca2c3abec0f5ac3fa992
SamBritt <sam.britt2@yahoo.com>
2020-08-19 12:16:01 -0500

    Initial commit using imageData to create pixels

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useRef, useEffect, useState } from 'react'
 import './App.css';
+import * as pressure from '../node_modules/pressure'
 
 function App() {
+
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const [isDrawing, setIsDrawing] = useState(false)
+  const [forceValue, setForceValue] = useState(0);
+
+  useEffect(() => {
+
+    const canvas = canvasRef.current;
+    canvas.width = 500;
+    canvas.height = 500;
+    canvas.style.width = `${500}px`;
+    canvas.style.height = `${500}px`;
+
+    const context = canvas.getContext("2d")
+
+    //Image data/pen stroke to add
+
+    context.scale(2, 2)
+    context.lineCap = "round"
+    context.strokeStyle = 'rgba(5,18,222,0)';
+    context.lineWidth = 5;
+    contextRef.current = context;
+  }, [])
+
+  const startDrawing = ({ nativeEvent }) => {
+
+    const { offsetX, offsetY } = nativeEvent;
+    console.log(nativeEvent)
+    //contextRef.current.beginPath()
+    let imageData = contextRef.current.createImageData(5, 5);
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      imageData.data[i + 0] = 255;
+      imageData.data[i + 1] = 0;
+      imageData.data[i + 2] = 0;
+      imageData.data[i + 3] = 255;
+    }
+    console.log(imageData)
+    //contextRef.current.moveTo(offsetX, offsetY)
+    contextRef.current.putImageData(imageData, offsetX, offsetY)
+    setIsDrawing(true)
+  }
+
+  const finishDrawing = () => {
+    contextRef.current.closePath()
+    setIsDrawing(false)
+  }
+  const pointerTest = ({ nativeEvent }) => {
+    const pressure = nativeEvent;
+    console.log(pressure.pressure)
+  }
+
+  const draw = ({ nativeEvent }) => {
+
+    if (!isDrawing) {
+      return
+    }
+    const { offsetX, offsetY, pressure } = nativeEvent;
+
+    //let p = contextRef.current.strokeStyle = 'rgba(5,18,222,' + `${pressure}` + ')'
+    //console.log(p)
+
+    let imageData = contextRef.current.createImageData(5, 5);
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      imageData.data[i + 0] = 255;
+      imageData.data[i + 1] = 0;
+      imageData.data[i + 2] = 0;
+      imageData.data[i + 3] = 255;
+    }
+    
+    //contextRef.current.moveTo(offsetX, offsetY)
+    contextRef.current.putImageData(imageData, offsetX, offsetY)
+    //contextRef.current.lineTo(offsetX, offsetY)
+    //contextRef.current.stroke()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <canvas id='canvasOne'
+      onPointerDown={startDrawing}
+      //onMouseDown={startDrawing}
+      //onMouseUp={finishDrawing}
+      onPointerUp={finishDrawing}
+      //onMouseMove={draw}
+      onPointerMove={draw}
+      ref={canvasRef}
+    />
   );
+
+
 }
 
 export default App;
